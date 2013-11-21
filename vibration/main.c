7931d592e9ecda208c00f2a3eafd05ec753ada81
Markus Schlafli <m_schlafli@hotmail.com>
2013-11-21 15:38:43 +0000

    Cleanup of temperature functionality

--- vibration/main.c
+++ vibration/main.c
@@ -4,6 +4,7 @@
 #include "nrf_temp.h"
 #include "nrf_soc.h"
 #include "nrf_delay.h"
+//#include "nrf_gpio.h"
 #include "twi_master.h"
 #include "app_timer.h"
 #include "libjanek.h"
@@ -57,7 +58,7 @@ Function: initialize_all(void)
 
 ==============================================
  */
-static void initialize_all(int8_t useTemperature) 
+static void initialize_all()
 {
 	// Start 16 MHz crystal oscillator.
 	NRF_CLOCK->EVENTS_HFCLKSTARTED = 0;
@@ -91,35 +92,104 @@ static void initialize_all(int8_t useTemperature)
 	simple_uart_putstring("INIT VIBRATION DONE\n");
 
 	// initialize temperature
-	if (useTemperature) {
-		nrf_temp_init();
-	}
+	nrf_temp_init();
 }
 
 /*
 ==============================================
-Function: main()
+Function: find_temperature( 
+			uint8_t * temperatureToReach, 
+			uint8_t * temperatureDifference, 
+			uint8_t * currentTemperature, 
+			int8_t * hasReachedTemperature
+			)
+		
+	Calls the function for finding temperature and 
+	checking if temperature has rising to a certain 
+	threshold.
 
-	Entry point of the application.
+Explanation:
+	First the base temperature is found. This is the 
+	first reading where two temperature readings are 
+	close to each other (+-2).
+
+	After the base temperature is found, each 
+	subsequent reading is tested against the temperature 
+	target. This target is calculated as follows:
+		target = base_temperature + temperature_difference
+
+	Once this target has been hit, the passed parameter 
+	"hasReachedTemperature" is changed from 0 to 1 to 
+	signal to the caller that the temperature target 
+	has indeed been hit.
 
 ==============================================
  */
+void find_temperature(uint8_t * temperatureToReach, uint8_t * temperatureDifference, 
+		uint8_t * currentTemperature, int8_t * hasReachedTemperature) 
+{
+	// get temperature
+	if (!*hasReachedTemperature) {
+
+		// This function contains workaround for PAN_028 rev2.0A anomalies 28, 29,30 and 31.
+	    	// stops the compiler optimizing anything to do with this variable
+	    	int32_t volatile temperatureTemp;
+
+        	NRF_TEMP->TASKS_START = 1;
+
+        	while (NRF_TEMP->EVENTS_DATARDY == 0)            
+		{
+       			// Do nothing.
+ 	      	}
+        	NRF_TEMP->EVENTS_DATARDY    = 0;  
+     
+		temperatureTemp                        = (nrf_temp_read()/4);
+        
+        	NRF_TEMP->TASKS_STOP        = 1; 
+
+		uint8_t temperature = (uint8_t)(temperatureTemp);
+		// debugging: print temperature
+		char temp_s[80];
+		sprintf(temp_s, "Temperature: %d\n", temperature);
+ 		simple_uart_putstring(temp_s);
+		
+		// either we find the base temperature or we check for 
+		// if the temperature has risen enough
+		if (*temperatureToReach != 0) {				
+			*currentTemperature = temperature;
+			// check if we've reached the correct temperature
+			if (*currentTemperature >= *temperatureToReach) {
+				nrf_gpio_pin_toggle(0); // testing: toggle LED when temperature has been reached
+				*hasReachedTemperature = 1;
+			}
+		} else {
+			// get the first reading
+			if (*currentTemperature == 0) {
+				*currentTemperature = temperature;	
+			} else {
+				// if current and previous reading are different by at most +- 2
+				// then we've found our base temperature
+				if (*currentTemperature<temperature+2 && *currentTemperature>temperature-2) {
+					*temperatureToReach = temperature + *temperatureDifference;
+					simple_uart_putstring("Found temperature to reach");
+				} 
+				*currentTemperature = temperature;
+			}
+		}
+	}
+}
 
 int main()
 {
-	// This function contains workaround for PAN_028 rev2.0A anomalies 28, 29,30 and 31.
-    	// stops the compiler optimizing anything to do with this variable
-    	int32_t volatile temperatureTemp;
-	uint8_t temperatureToReach = 100;
-	uint8_t currentTemperature;
+	// temperature variables
+	uint8_t temperatureToReach = 0;
+	uint8_t temperatureDifference = 10;
+	uint8_t currentTemperature = 0;
 	int8_t hasReachedTemperature = 0;
-	int8_t useTemperature = 0;
+	int8_t useTemperature = 1;
     
-	// the following commmented line starts the led range and was included in the sample temperature file
-    	//nrf_gpio_range_cfg_output(LED_START, LED_STOP);
-
 		// Initialize
-		initialize_all(useTemperature);
+		initialize_all();
 
 		// Enable LED
 		nrf_gpio_cfg_output(0);
@@ -132,39 +202,15 @@ int main()
 		
 		uint8_t uart_data;
 		char* buf[30];
+
 		while (1) {
 
 			if (useTemperature) {
-			// get temperature
-			if (!hasReachedTemperature) {
-		        	NRF_TEMP->TASKS_START = 1; /** Start the temperature measurement. */
-
-        			/* Busy wait while temperature measurement is not finished, you can skip waiting if you enable interrupt for DATARDY event and read the result in the interrupt. */
-		        	/*lint -e{845} // A zero has been given as right argument to operator '|'" */
-		        	while (NRF_TEMP->EVENTS_DATARDY == 0)            
-        			{
-            				// Do nothing.
-		        	}
-		        	NRF_TEMP->EVENTS_DATARDY    = 0;  
-        
-	       			/**@note Workaround for PAN_028 rev2.0A anomaly 29 - TEMP: Stop task clears the TEMP register. */       
-       	 			temperatureTemp                        = (nrf_temp_read()/4);
-        
-			        /**@note Workaround for PAN_028 rev2.0A anomaly 30 - TEMP: Temp module analog front end does not power down when DATARDY event occurs. */
-		        	NRF_TEMP->TASKS_STOP        = 1; /** Stop the temperature measurement. */
-
-				// the follpowing line is from the sample temperature file. It writes the temperature to a given gpio port.
-	        		//nrf_gpio_port_write(NRF_GPIO_PORT_SELECT_PORT1, (uint8_t)(temperatureTmp));
-
-				currentTemperature = (uint8_t)(temperatureTemp);
-				
-				// check if we've reached the correct temperature
-				if (currentTemperature >= temperatureToReach) {
-					hasReachedTemperature = 1;
-				}
-			}
+				find_temperature(&temperatureToReach, &temperatureDifference, 
+					&currentTemperature, &hasReachedTemperature);	
 			}
 
+			
 			if ( is_connected() ) {
 				simple_uart_putstring("IMCONNECTED !!dd!\n");
 				nrf_gpio_pin_toggle(0);
@@ -203,8 +249,9 @@ int main()
 				        sd_softdevice_enable(NRF_CLOCK_LFCLKSRC_RC_250_PPM_1000MS_CALIBRATION,app_error_handler);
 					simple_uart_putstring("enable soft device\n");
 			}
-			vibration_update();
-			app_sched_execute(); 
+		
+			//vibration_update();
+			//app_sched_execute(); 
 				//power_manage(); // hangs, while loop stops here
 			nrf_delay_ms(500);
 		}
