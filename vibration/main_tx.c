bdc98514cd038ccd2c859434ce49aad99c671347
Markus Schlafli <m_schlafli@hotmail.com>
2013-11-21 17:09:03 +0000

    Added temperature functionality to kettle main.

--- vibration/main_tx.c
+++ vibration/main_tx.c
@@ -3,6 +3,7 @@
 #include <math.h>
 #include "nrf_soc.h"
 #include "nrf_delay.h"
+#include "nrf_temp.h"
 #include "twi_master.h"
 #include "app_timer.h"
 #include "libjanek.h"
@@ -14,6 +15,90 @@
 #include "app_scheduler.h"
 
 static uint64_t packet[PACKET_PAYLOAD_MAXSIZE];  // Packet to transmit.
+
+/*
+==============================================
+Function: find_temperature( 
+			uint8_t * temperatureToReach, 
+			uint8_t * temperatureDifference, 
+			uint8_t * currentTemperature, 
+			int8_t * hasReachedTemperature
+			)
+		
+	Calls the function for finding temperature and 
+	checking if temperature has rising to a certain 
+	threshold.
+
+Explanation:
+	First the base temperature is found. This is the 
+	first reading where two temperature readings are 
+	close to each other (+-2).
+
+	After the base temperature is found, each 
+	subsequent reading is tested against the temperature 
+	target. This target is calculated as follows:
+		target = base_temperature + temperature_difference
+
+	Once this target has been hit, the passed parameter 
+	"hasReachedTemperature" is changed from 0 to 1 to 
+	signal to the caller that the temperature target 
+	has indeed been hit.
+
+==============================================
+ */
+void find_temperature(uint8_t * temperatureToReach, uint8_t * temperatureDifference, 
+		uint8_t * currentTemperature, int8_t * hasReachedTemperature) 
+{
+	// get temperature
+	if (!*hasReachedTemperature) {
+
+		// This function contains workaround for PAN_028 rev2.0A anomalies 28, 29,30 and 31.
+		// stops the compiler optimizing anything to do with this variable
+		int32_t volatile temperatureTemp;
+
+        	NRF_TEMP->TASKS_START = 1;
+
+        	while (NRF_TEMP->EVENTS_DATARDY == 0)            
+		{
+			// Do nothing.
+		}
+        	NRF_TEMP->EVENTS_DATARDY    = 0;  
+     
+		temperatureTemp             = (nrf_temp_read()/4);
+        
+        	NRF_TEMP->TASKS_STOP        = 1; 
+
+		uint8_t temperature = (uint8_t)(temperatureTemp);
+		// debugging: print temperature
+		char temp_s[80];
+		sprintf(temp_s, "Temperature: %d\n", temperature);
+ 		simple_uart_putstring(temp_s);
+		
+		// either we find the base temperature or we check for 
+		// if the temperature has risen enough
+		if (*temperatureToReach != 0) {				
+			*currentTemperature = temperature;
+			// check if we've reached the correct temperature
+			if (*currentTemperature >= *temperatureToReach) {
+				nrf_gpio_pin_toggle(0); // testing: toggle LED when temperature has been reached
+				*hasReachedTemperature = 1;
+			}
+		} else {
+			// get the first reading
+			if (*currentTemperature == 0) {
+				*currentTemperature = temperature;	
+			} else {
+				// if current and previous reading are different by at most +- 2
+				// then we've found our base temperature
+				if (*currentTemperature<temperature+2 && *currentTemperature>temperature-2) {
+					*temperatureToReach = temperature + *temperatureDifference;
+					simple_uart_putstring("Found temperature to reach");
+				} 
+				*currentTemperature = temperature;
+			}
+		}
+	}
+}
 
 
 void app_error_handler(uint32_t error_code, uint32_t line_num, const uint8_t * p_file_name)
@@ -50,6 +135,16 @@ void send_packet(uint8_t rep)
 
 int main()
 {
+	// temperature variables
+	uint8_t temperatureToReach = 0;
+	uint8_t temperatureDifference = 10;
+	uint8_t currentTemperature = 0;
+	int8_t hasReachedTemperature = 0;
+	int8_t useTemperature = 0;
+
+	// initialize temperature
+	nrf_temp_init();
+
 	// Start 16 MHz crystal oscillator.
 	NRF_CLOCK->EVENTS_HFCLKSTARTED = 0;
 	NRF_CLOCK->TASKS_HFCLKSTART    = 1;
@@ -86,7 +181,15 @@ int main()
 	bool sending = false;
 	bool receiving = false;
 
+	// main application loop
 	while (1) {
+
+		// Check the temperature rise.
+		if (useTemperature) {
+			find_temperature(&temperatureToReach, &temperatureDifference, 
+				&currentTemperature, &hasReachedTemperature);	
+		}
+
 		if (receiving) {
 			simple_uart_putstring("Ready to receive!\n");
 			NRF_RADIO->PACKETPTR    = &packet; // Set payload pointer.
